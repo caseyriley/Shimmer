c8aacb0269fa223ef32e641b1fa3126ea4134b56
Casey <innerforest7@gmail.com>
2020-08-06 10:37:52 -0700

    remove helmet

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const createError = require('http-errors');
 const express = require('express');
-const helmet = require('helmet');
+// const helmet = require('helmet');
 const path = require('path');
 const logger = require('morgan');
 
@@ -10,7 +10,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors({ origin: true }));
-app.use(helmet({ hsts: false }));
+// app.use(helmet({ hsts: false }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
