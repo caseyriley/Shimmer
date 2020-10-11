ca43ee9650b304aad3a755980bbef4807b7adff3
Casey <innerforest7@gmail.com>
2020-10-10 21:44:58 -0700

    add #hide

--- client/src/components/YourGallery.js
+++ client/src/components/YourGallery.js
@@ -28,8 +28,10 @@ const YourGallery = (props) => {
         <div id="your-gallery" className={`${props.galleryPageState ? 'page-showing' : 'page-hidden'}`}>
         {/* <div id="your-gallery" className='page-showing'> */}
             {/* <NavBar></NavBar> */}
-            <div id="your-gallery-top">
-                <div onClick={props.hideGallery}> <ion-icon id="your-gallery__back-arrow" name="arrow-back-outline"></ion-icon> <p id="your-gallery__back-arrow_text" >Back to explore</p> </div>
+            <div id={`${props.galleryPageState === true ? "your-gallery-top" : "hide"}`}>
+                <div onClick={props.hideGallery}> <ion-icon id={"your-gallery__back-arrow"} name="arrow-back-outline"></ion-icon> 
+                  <p id="your-gallery__back-arrow_text" >Back to explore</p> 
+                </div>
                 <p id="your-gallery__title">Your Gallery</p>
                 <p id="your-gallery__description">a gallery currated by you!</p>
             </div>
